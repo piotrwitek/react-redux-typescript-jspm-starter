4aae89fa0f767ac221403a9d46d1f31f077abaa4
Piotr Witek <piotrek.witek@gmail.com>
2016-07-10 14:56:52 +0200

    added - custom ambient definitions for external libraries best-practice example

---
+++ src/app-typings.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/index.d.ts" />
+
+declare const System: any;
+
+// declare module 'module-name' {
+//   export const example: any;
+//   export default { m: example };
+// }
