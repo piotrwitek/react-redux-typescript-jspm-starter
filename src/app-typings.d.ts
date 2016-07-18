52e1170cfb4851a5451d5a5643c9701e1054e378
Piotr Witek <piotrek.witek@gmail.com>
2016-07-18 16:37:33 +0200

    added example of external React Component declaration

--- src/app-typings.d.ts
+++ src/app-typings.d.ts
@@ -1,10 +1,29 @@
 declare const System: any;
 
+// example ES Module declaration
 declare module 'example-module' {
   export const exampleVariable: number;
   export function exampleFunction(param: string): boolean;
   export default {
     exampleVariable: exampleVariable,
     exampleFunction: exampleFunction
+  };
+}
+
+// example of external React Component declaration
+declare module 'example-react-external-component' {
+  import * as React from 'react';
+
+  export function fetchData(name: string): Promise<string[]>;
+
+  export class ReactExternalComponent extends React.Component<IProps, any> { }
+  interface IProps {
+    name: string;
+    age: number;
+  }
+
+  export default {
+    fetchData: fetchData,
+    ReactExternalComponent: ReactExternalComponent
   };
 }
