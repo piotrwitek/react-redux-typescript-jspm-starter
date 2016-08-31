eab377f06947a6da44cabf02f394fe5a5bdb2be1
Piotr Witek <piotrek.witek@gmail.com>
2016-08-31 10:49:03 +0200

    updated linter config

--- src/components/page-section.tsx
+++ src/components/page-section.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import classNames from 'classnames';
 
-const inlineStyles = {
-  minHeight: { minHeight: '200px' }
-};
+// const inlineStyles = {};
 
 export function PageSection({className = '', children = undefined}) {
 
