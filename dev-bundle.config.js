b425a72277ec4ec2b21857b44730fc4413e4dd15
Piotr Witek <piotrek.witek@gmail.com>
2016-08-31 09:53:25 +0200

    updated dev bundle config

--- dev-bundle.config.js
+++ dev-bundle.config.js
@@ -1,6 +1,12 @@
 // importing libs should be included in dev-bundle
-import 'react';
-import 'react-dom';
 import 'plugin-typescript';
 import 'css';
 import 'systemjs-hot-reloader';
+import 'react';
+import 'react-dom';
+import 'react-redux';
+import 'react-router';
+import 'react-router-redux';
+import 'redux-actions';
+import 'classnames';
+import 'seamless-immutable';
