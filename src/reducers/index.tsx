c60353ae1620513554268dae820b7836a87375eb
Piotr Witek <piotrek.witek@gmail.com>
2016-09-02 19:09:10 +0200

    added redux devtools integration aka Time Travel Debugging

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -4,13 +4,8 @@ import { routerReducer } from 'react-router-redux';
 import currencyRatesReducer from './currency-rates-reducer';
 import currencyConverterReducer from './currency-converter-reducer';
 
-export const initialState = {
-};
-
-export const rootReducer = combineReducers({
+export const reducers = combineReducers({
   routing: routerReducer,
   currencyRates: currencyRatesReducer,
   currencyConverter: currencyConverterReducer
 });
-
-export const hotReloadReducer = (state, action) => action.payload;
--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,28 @@
 import { createStore } from 'redux';
-import { initialState, rootReducer } from './reducers/index';
+import { reducers } from './reducers/index';
+declare const window: any;
 
-export const store = createStore(rootReducer, initialState);
+const recoverState = () => ({});
+
+export const store = createStore(
+  reducers,
+  recoverState(),
+  window.devToolsExtension && window.devToolsExtension()
+);
 
 // hot-reload hook, rehydrating the state of redux store
 export function __reload(exports) {
   console.log(exports.store.getState());
-  // if (exports.store) {
-  //   const localStore = exports.store;
-  //   const prevState = localStore.getState();
-  //
-  //   localStore.replaceReducer(hotReloadReducer);
-  //   localStore.dispatch({ type: null, payload: prevState });
-  //   // console.log('store rehydrated', localStore.getState());
-  //
-  //   localStore.replaceReducer(rootReducer);
-  // }
 }
+
+// custom hot-reload using reducer replacement
+// const hotReloadReducer = (state, action) => action.payload;
+// if (exports.store) {
+//   const localStore = exports.store;
+//   const prevState = localStore.getState();
+//
+//   localStore.replaceReducer(hotReloadReducer);
+//   localStore.dispatch({ type: null, payload: prevState });
+//   console.log('store rehydrated', localStore.getState());
+//
+//   localStore.replaceReducer(rootReducer);
