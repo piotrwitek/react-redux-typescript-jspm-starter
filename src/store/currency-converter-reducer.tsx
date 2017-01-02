9a1f44cd19ad1ab92b33bd5655be2f7cc59b0c46
Piotr Witek <piotrek.witek@gmail.com>
2017-01-03 00:45:59 +0100

    refactored reducers

--- src/store/currency-converter-reducer.tsx
+++ src/store/currency-converter-reducer.tsx
@@ -1,17 +1,28 @@
-import { createAction, Action } from 'redux-actions';
-import * as Immutable from 'seamless-immutable';
-
 // Action Types - LOAD, CREATE, UPDATE, REMOVE
 const UPDATE_BASE_CURRENCY = 'currencyConverter/UPDATE_BASE_CURRENCY';
 const UPDATE_TARGET_CURRENCY = 'currencyConverter/UPDATE_TARGET_CURRENCY';
 const UPDATE_BASE_VALUE = 'currencyConverter/UPDATE_BASE_VALUE';
 const UPDATE_TARGET_VALUE = 'currencyConverter/UPDATE_TARGET_VALUE';
 
+// extract to react-redux-typescript
+declare interface ObjectConstructor {
+  assign<T>(target: {}, source: T, ...sources: Partial<T>[]): T;
+}
+declare const Object: ObjectConstructor;
+export function getAction<P, A>(actionCreator: (payload: P) => A): A {
+  return {} as false || {} as A;
+}
+
 // Action Creators
-export const updateBaseCurrency = createAction<string>(UPDATE_BASE_CURRENCY);
-export const updateTargetCurrency = createAction<string>(UPDATE_TARGET_CURRENCY);
-export const updateBaseValue = createAction<string>(UPDATE_BASE_VALUE);
-export const updateTargetValue = createAction<string>(UPDATE_TARGET_VALUE);
+// TODO: add type definition inference to type prop, should not be string
+export const updateBaseCurrency = (payload: string) => ({ type: UPDATE_BASE_CURRENCY, payload });
+export const updateTargetCurrency = (payload: string) => ({ type: UPDATE_TARGET_CURRENCY, payload });
+export const updateBaseValue = (payload: string) => ({ type: UPDATE_BASE_VALUE, payload });
+export const updateTargetValue = (payload: string) => ({ type: UPDATE_TARGET_VALUE, payload });
+
+const a = getAction(updateBaseCurrency);
+const b = getAction(updateTargetCurrency);
+type Actions = typeof a | typeof b;
 
 // Reducer
 export interface ICurrencyConverterReducer {
@@ -27,22 +38,22 @@ const initialState: ICurrencyConverterReducer = {
   targetValue: '0',
 };
 
-export default function reducer(state = Immutable.from(initialState), action: Action<any>) {
+export default function reducer(state = initialState, action: Actions) {
   switch (action.type) {
     case UPDATE_BASE_CURRENCY:
-      return state.merge({
+      return Object.assign({}, state, {
         baseCurrency: action.payload,
       });
     case UPDATE_TARGET_CURRENCY:
-      return state.merge({
+      return Object.assign({}, state, {
         targetCurrency: action.payload,
       });
     case UPDATE_BASE_VALUE:
-      return state.merge({
+      return Object.assign({}, state, {
         baseValue: action.payload,
       });
     case UPDATE_TARGET_VALUE:
-      return state.merge({
+      return Object.assign({}, state, {
         targetValue: action.payload,
       });
 
--- src/store/currency-rates-reducer.tsx
+++ src/store/currency-rates-reducer.tsx
@@ -1,6 +1,3 @@
-import { handleActions, Action } from 'redux-actions';
-import * as Immutable from 'seamless-immutable';
-
 const RESPONSE_MOCK: IFixerServiceResponse = {
   base: 'PLN',
   date: Date.now().toString(),
@@ -14,15 +11,12 @@ const LOAD_CURRENCY_RATES_ERROR = 'currencyRates/LOAD_CURRENCY_RATES_ERROR';
 
 // Action Creators
 export const loadCurrencyRates = () => ({ type: LOAD_CURRENCY_RATES });
-export const loadCurrencyRatesSuccess = (payload: any) => ({
-  type: LOAD_CURRENCY_RATES_SUCCESS,
-  payload: payload,
+export const loadCurrencyRatesSuccess = (payload: IFixerServiceResponse) => ({
+  type: LOAD_CURRENCY_RATES_SUCCESS, payload: payload,
 });
-export const loadCurrencyRatesError = (payload: any) => ({
-  type: LOAD_CURRENCY_RATES_ERROR,
-  payload: payload,
+export const loadCurrencyRatesError = (payload: string) => ({
+  type: LOAD_CURRENCY_RATES_ERROR, payload: payload,
 });
-
 
 // Reducer
 export interface ICurrencyRatesReducer {
@@ -32,27 +26,33 @@ export interface ICurrencyRatesReducer {
   base: string;
   rates: any;
 }
-
-const initialState = Immutable.from<ICurrencyRatesReducer>({
+const initialState: ICurrencyRatesReducer = {
   isLoading: false,
   errorMessage: null,
   lastUpdated: null,
   base: 'PLN',
   rates: RESPONSE_MOCK.rates,
-});
+};
 
-export default handleActions<any, any>({
-  [LOAD_CURRENCY_RATES]: (state: typeof initialState, action: Action<void>) => state.merge({
-    isLoading: true,
-  }),
-  [LOAD_CURRENCY_RATES_SUCCESS]: (state: typeof initialState, action: Action<IFixerServiceResponse>) => state.merge({
-    isLoading: false,
-    errorMessage: null,
-    rates: action.payload && action.payload.rates,
-    lastUpdated: Date.now(),
-  }),
-  [LOAD_CURRENCY_RATES_ERROR]: (state: typeof initialState, action: Action<string>) => state.merge({
-    isLoading: false,
-    errorMessage: action.payload,
-  }),
-}, initialState);
+export default function reducer(state = initialState, action: any) {
+  switch (action.type) {
+    case LOAD_CURRENCY_RATES:
+      return Object.assign({}, state, {
+        isLoading: true,
+      });
+    case LOAD_CURRENCY_RATES_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        errorMessage: null,
+        rates: action.payload && action.payload.rates,
+        lastUpdated: Date.now(),
+      });
+    case LOAD_CURRENCY_RATES_ERROR:
+      return Object.assign({}, state, {
+        isLoading: false,
+        errorMessage: action.payload,
+      });
+
+    default: return state;
+  };
+}
