0ce17fdc9ff760323487f142f2b7ec9d9f6a8a29
Piotr Witek <piotrek.witek@gmail.com>
2017-01-05 02:24:12 +0100

    better type safety with minimal boilerplate

--- src/store/currency-converter-reducer.tsx
+++ src/store/currency-converter-reducer.tsx
@@ -1,53 +1,65 @@
 // Action Types - LOAD, CREATE, UPDATE, REMOVE
-const UPDATE_BASE_CURRENCY = 'currencyConverter/UPDATE_BASE_CURRENCY';
-const UPDATE_TARGET_CURRENCY = 'currencyConverter/UPDATE_TARGET_CURRENCY';
-const UPDATE_BASE_VALUE = 'currencyConverter/UPDATE_BASE_VALUE';
-const UPDATE_TARGET_VALUE = 'currencyConverter/UPDATE_TARGET_VALUE';
-
-export function getAction<P, A>(actionCreator: (payload: P) => A): A {
-  return {} as false || {} as A;
+enum ActionTypes {
+  UPDATE_BASE_CURRENCY, UPDATE_TARGET_CURRENCY,
+  UPDATE_BASE_VALUE, UPDATE_TARGET_VALUE,
 }
 
+type ActionsUnion =
+  {
+    type: ActionTypes.UPDATE_BASE_CURRENCY, payload: string,
+  } | {
+    type: ActionTypes.UPDATE_TARGET_CURRENCY, payload: string,
+  } | {
+    type: ActionTypes.UPDATE_BASE_VALUE, payload: string,
+  } | {
+    type: ActionTypes.UPDATE_TARGET_VALUE, payload: string,
+  };
+
+
 // Action Creators
-// TODO: add type definition inference to type prop, should not be string
-export const updateBaseCurrency = (payload: string) => ({ type: UPDATE_BASE_CURRENCY, payload });
-export const updateTargetCurrency = (payload: string) => ({ type: UPDATE_TARGET_CURRENCY, payload });
-export const updateBaseValue = (payload: string) => ({ type: UPDATE_BASE_VALUE, payload });
-export const updateTargetValue = (payload: string) => ({ type: UPDATE_TARGET_VALUE, payload });
+export const updateBaseCurrency = (payload: string): ActionsUnion => ({
+  type: ActionTypes.UPDATE_BASE_CURRENCY, payload,
+});
+export const updateTargetCurrency = (payload: string): ActionsUnion => ({
+  type: ActionTypes.UPDATE_TARGET_CURRENCY, payload,
+});
+export const updateBaseValue = (payload: string): ActionsUnion => ({
+  type: ActionTypes.UPDATE_BASE_VALUE, payload,
+});
+export const updateTargetValue = (payload: string): ActionsUnion => ({
+  type: ActionTypes.UPDATE_TARGET_VALUE, payload,
+});
 
-const a = getAction(updateBaseCurrency);
-const b = getAction(updateTargetCurrency);
-type Actions = typeof a | typeof b;
-
-// Reducer
+// State
 export interface ICurrencyConverterReducer {
   baseCurrency: string;
   targetCurrency: string;
   baseValue: string;
   targetValue: string;
 }
-const initialState: ICurrencyConverterReducer = {
+export const initialState: ICurrencyConverterReducer = {
   baseCurrency: 'PLN',
   targetCurrency: 'SEK',
   baseValue: '0',
   targetValue: '0',
 };
 
-export default function reducer(state = initialState, action: Actions): ICurrencyConverterReducer {
+// Reducer
+export default function reducer(state = initialState, action: ActionsUnion): ICurrencyConverterReducer {
   switch (action.type) {
-    case UPDATE_BASE_CURRENCY:
+    case ActionTypes.UPDATE_BASE_CURRENCY:
       return {
         ...state, baseCurrency: action.payload,
       };
-    case UPDATE_TARGET_CURRENCY:
+    case ActionTypes.UPDATE_TARGET_CURRENCY:
       return {
         ...state, targetCurrency: action.payload,
       };
-    case UPDATE_BASE_VALUE:
+    case ActionTypes.UPDATE_BASE_VALUE:
       return {
         ...state, baseValue: action.payload,
       };
-    case UPDATE_TARGET_VALUE:
+    case ActionTypes.UPDATE_TARGET_VALUE:
       return {
         ...state, targetValue: action.payload,
       };
